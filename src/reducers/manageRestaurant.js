a65b87d46487b1fa86306586b0a0f4a65d939057
Luke Ghenco <lukeghenco@gmail.com>
2017-02-10 08:33:43 -0500

    update manageRestaurants reducer

--- src/reducers/manageRestaurant.js
+++ src/reducers/manageRestaurant.js
@@ -1,24 +1,34 @@
 import cuid from 'cuid';
-export const cuidFn = cuid
-export default function manageRestaurants(state = {restaurants: [], reviews: []}, action){
+export const cuidFn = cuid;
+
+export default function manageRestaurants(state = {
+  restaurants: [],
+  reviews: [],
+}, action) {
   switch (action.type) {
+    
     case 'ADD_RESTAURANT':
-      let restaurant = Object.assign({}, {text: action.payload}, {id: cuidFn()})
-      return Object.assign(state, {}, {restaurants: state.restaurants.concat(restaurant)})
+      const restaurant = { text: action.restaurant.text, id: cuidFn() };
+      return Object.assign({}, state, { 
+        restaurants: state.restaurants.concat(restaurant),
+      });
+
     case 'DELETE_RESTAURANT':
-      let restaurants = state.restaurants.filter(function(restaurant){
-        return restaurant.id !== action.id
-      })
-      return Object.assign({}, state, {restaurants: restaurants})
-      case 'ADD_REVIEW':
-        let review = Object.assign({}, {text: action.payload.text, restaurantId: action.payload.restaurantId}, {id: cuidFn()})
-        return Object.assign({}, state, {reviews: state.reviews.concat(review)})
-      case 'DELETE_REVIEW':
-        let reviews = state.reviews.filter(function(review){
-          return review.id !== action.id
-        })
-      return Object.assign({}, state, {reviews: reviews})
-    default:
+      const restaurants = state.restaurants.filter(restaurant => restaurant.id !== action.id);
+      return Object.assign({}, state, { restaurants });
+
+    case 'ADD_REVIEW':
+      const review = Object.assign({}, action.review, { id: cuidFn() });
+      return Object.assign({}, state, {
+        reviews: state.reviews.concat(review),
+      });
+
+    case 'DELETE_REVIEW':
+      const reviews = state.reviews.filter(review => review.id !== action.id);
+      return Object.assign({}, state, { reviews });
+
+    default: 
       return state;
+
   }
-}
+};
