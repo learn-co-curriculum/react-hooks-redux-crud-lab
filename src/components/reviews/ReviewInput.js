94d64a0f4c60369fa0785940fcd07ff39c1d3db4
Luke Ghenco <lukeghenco@gmail.com>
2017-02-10 08:36:54 -0500

    update ReviewInput solution

--- src/components/reviews/ReviewInput.js
+++ src/components/reviews/ReviewInput.js
@@ -1,30 +1,53 @@
-import React, { Component } from 'react'
-import Reviews from './Reviews'
+import React, { Component } from 'react';
+import Reviews from './Reviews';
 
 class ReviewInput extends Component {
-  constructor(props){
-    super(props)
-    this.state = {text: ''}
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      text: '',
+    };
   }
-  handleOnChange(event){
-    this.setState({text: event.target.value})
+
+  handleOnChange(event) {
+    this.setState({
+      text: event.target.value,
+    });
   }
-  handleOnSubmit(event){
-    event.preventDefault()
-    this.props.store.dispatch({type: 'ADD_REVIEW', payload: {text: this.state.text, restaurantId: this.props.restaurantId}})
-    this.setState({text: ''})
+
+  handleOnSubmit(event) {
+    event.preventDefault();
+    this.props.store.dispatch({
+      type: 'ADD_REVIEW',
+      review: {
+        text: this.state.text,
+        restaurantId: this.props.restaurantId,
+      },
+    });
+    this.setState({
+      text: '',
+    });
   }
-  render(){
-    return (<div>
-      <form onSubmit={this.handleOnSubmit.bind(this)}>
-        <label> Add Review </label>
-        <input type="text" value={this.state.text} onChange={this.handleOnChange.bind(this)}/>
-        <input type="submit" />
-        <Reviews store={this.props.store} restaurantId={this.props.restaurantId} />
-      </form>
-    </div>
-    )
+
+  render() {
+    return (
+     <div>
+        <form onSubmit={(event) => this.handleOnSubmit(event)} >
+          <label>Add Review</label>
+          <input 
+            type="text"
+            value={this.state.text}
+            onChange={(event) => this.handleOnChange(event)} />
+          <input type="submit" />
+        </form>
+        <Reviews 
+          store={this.props.store} 
+          restaurantId={this.props.restaurantId} />
+      </div>
+    );
   }
-}
+};
 
 export default ReviewInput;
