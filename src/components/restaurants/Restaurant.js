6012fe666481a15ca95fe039f22c0d4cfedd9698
Luke Ghenco <lukeghenco@gmail.com>
2017-02-10 08:35:01 -0500

    update Restaurant component solution

--- src/components/restaurants/Restaurant.js
+++ src/components/restaurants/Restaurant.js
@@ -1,19 +1,30 @@
-import React, { Component } from 'react'
-import ReviewInput from '../reviews/ReviewInput'
+import React, { Component } from 'react';
+import ReviewInput from '../reviews/ReviewInput';
 
 class Restaurant extends Component {
-  handleOnClick(){
-    this.props.store.dispatch({type: 'DELETE_RESTAURANT', id: this.props.id})
-  }
-  render(){
-    return(
-      <li>
-        {this.props.text}
-        <button onClick={this.handleOnClick.bind(this)}> X </button>
-        < ReviewInput store={this.props.store} restaurantId={this.props.id} />
-      </li>
-    )
-  }
-}
 
-export default Restaurant
+  handleOnClick() {
+    this.props.store.dispatch({
+      type: 'DELETE_RESTAURANT',
+      id: this.props.restaurant.id
+    });
+  }
+  
+  render() {
+    const { text, id } = this.props.restaurant;
+
+    return (
+      <div>
+        <li>
+          {text}
+          <button onClick={() => this.handleOnClick()}> X </button>
+          <ReviewInput 
+            store={this.props.store}
+            restaurantId={id} />
+        </li>
+      </div>
+    );
+  }
+};
+
+export default Restaurant;
