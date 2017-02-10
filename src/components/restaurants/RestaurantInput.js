46246b672220ad2b88818c08cf97e2e07baeaf95
Luke Ghenco <lukeghenco@gmail.com>
2017-02-10 08:35:31 -0500

    Update RestaurantInput solution

--- src/components/restaurants/RestaurantInput.js
+++ src/components/restaurants/RestaurantInput.js
@@ -1,27 +1,47 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 class RestaurantInput extends Component {
-  constructor(props){
-    super(props)
-    this.state = {text: ''}
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      text: ''
+    };
+  };
+
+  handleOnChange(event) {
+    this.setState({
+      text: event.target.value,
+    });
   }
-  handleOnChange(event){
-    this.setState({text: event.target.value})
+
+  handleOnSubmit(event) {
+    event.preventDefault();
+    this.props.store.dispatch({
+      type: 'ADD_RESTAURANT',
+      restaurant: {
+        text: this.state.text,
+      },
+    });
+    this.setState({
+      text: ''
+    });
   }
-  handleOnSubmit(event){
-    event.preventDefault()
-    this.props.store.dispatch({type: 'ADD_RESTAURANT', payload: this.state.text})
-    this.setState({text: ''})
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={(event) => this.handleOnSubmit(event)} >
+          <input 
+            type="text"
+            value={this.state.text}
+            onChange={(event) => this.handleOnChange(event)} />
+          <input type="submit" />
+        </form>
+      </div>
+    );
   }
-  render(){
-    return (<div>
-      <form onSubmit={this.handleOnSubmit.bind(this)}>
-        <input type="text" value={this.state.text} onChange={this.handleOnChange.bind(this)}/>
-        <input type="submit" />
-      </form>
-    </div>
-    )
-  }
-}
+};
 
 export default RestaurantInput;
